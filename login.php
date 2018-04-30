bb196b827f65a211b896261baf977127e4bc1b6f
alexis <alexismartin@live.fr>
2018-04-30 17:50:30 +0200

    Signup + debut login

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+
+	session_start();
+	if(isset($_POST['submit'])) {
+		include_once 'database.php';
+		$id = mysqli_real_escape_string($conn, $_POST['id']);
+		$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+		
+		if(empty($id) || empty($pwd)) {
+			header("Location: index.php?error=loginEmpty");
+			exit();
+		}
+		else {
+			$sql = "SELECT * FROM `user` WHERE MailUser = '$id' OR Pseudo = '$id'";
+			$result1 = mysqli_query($conn, $sql);
+			$res = mysqli_fetch_assoc($result1);
+			if(!$res){
+				header("Location: index.php?error=loginError");
+				exit();
+			}
+			if(mysqli_num_rows($result1) < 1) {
+				header("Location: index.php?error=loginError");
+				exit();
+			}
+			else {
+				$sql = "SELECT * FROM choucroutte WHERE User = '$res['IDUser']'";
+				$result = mysqli_query($conn, $sql);
+				if($res) {
+					$check = password_verify($pwd, $res['Hash']);
+					if($check==true) {
+						$row = mysqli_fetch_assoc($result1);
+						$_SESSION['id'] = $res['IDUser'];
+						header("Location: index.php?error=success");
+						exit();
+					}
+					else {
+						header("Location: index.php?error=loginError");
+						exit();
+					}
+				}
+			}
+		}
+	}
+	else {
+		header("Location: index.php?error=loginError");
+		exit();
+	}
---
+++ signup.php
@@ -0,0 +1,45 @@
+<?php
+	if(!isset($_POST['submit'])) {
+		header("Location: index.php");
+		exit();
+	}
+
+	include_once 'database.php';
+
+	$firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
+	$lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+	$pwd2 = mysqli_real_escape_string($conn, $_POST['pwd2']);
+	$pseudo = mysqli_real_escape_string($conn, $_POST['pseudo']);
+	
+	if(empty($firstName) || empty($lastName) || empty($email) || empty($pwd) || empty($pseudo)) {
+		header("Location: index.php?error=emptyField");
+		exit();
+	}
+	else {
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			header("Location: index.php?error=invalidEmail");
+			exit();
+		}
+		else if($pwd != $pwd2){
+			header("Location: index.php?error=wrongPassword");
+			exit();
+		}
+		else{
+			$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+			$sql = "INSERT INTO `user` (NameUser, FirstNameUser, MailUser, Admin, Pseudo) VALUES ('$lastName', '$firstName', '$email', False, '$pseudo');";
+			echo '$sql';
+			$result = mysqli_query($conn, $sql);
+			if($result == false) {
+				header("Location: index.php?error=existingAccount");
+				exit();
+			}
+			$result = mysqli_query($conn, "SELECT IDUser FROM `user` WHERE user.MailUser = '$email'");
+			$IDUser = mysqli_fetch_assoc($result)['IDUser'];
+			$sql = "INSERT INTO choucroutte (User, Hash) VALUES ('$IDUser', '$hashedPwd')";
+			$result = mysqli_query($conn, $sql);
+			header("Location: index.php?error=success");
+			exit();
+		}
+	}
