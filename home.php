5e5c4e23767d754dbe4a9f7d09272989f82886c3
alexis <alexismartin@live.fr>
2018-05-04 11:01:06 +0200

    submit post

--- home.php
+++ home.php
@@ -6,13 +6,18 @@
 	}
 ?>
 <div class = "newPublish">
-	<form method="post" action="newPost.php" method="POST">
-		<label>Insérer un document</label>
+	<form action="newPost.php" method="POST" enctype="multipart/form-data">
+		<label>Insérer une photo/courte video</label>
+		<input type="file" name="doc">
+		<label>Insérer la date</label>
+		<input type="date" name="date">
+		<label>Insérer le lieu</label>
+		<input type="text" name="place">
+		<label>Texte</label>
 		<textarea name="text", placeholder = "Ecrivez votre nouvelle publication ici"></textarea>
 		<button action="logout.php" method="POST" name="submit">Publier</button>
 	</form>
 </div>
-
 <?php
 	include_once "footer.php";
 ?>
